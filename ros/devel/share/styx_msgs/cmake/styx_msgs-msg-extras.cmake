04162367053f6a91a53cae5d9bbe6beb9109e187
Jason Huh <hankey0@gmail.com>
2019-04-18 17:33:46 -0700

    Remove build and devel from git and update .gitignore

--- ros/devel/share/styx_msgs/cmake/styx_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(styx_msgs_MESSAGE_FILES "/home/hankey0/Documents/Learning/CarND/CarND-Capstone/ros/src/styx_msgs/msg/TrafficLight.msg;/home/hankey0/Documents/Learning/CarND/CarND-Capstone/ros/src/styx_msgs/msg/TrafficLightArray.msg;/home/hankey0/Documents/Learning/CarND/CarND-Capstone/ros/src/styx_msgs/msg/Waypoint.msg;/home/hankey0/Documents/Learning/CarND/CarND-Capstone/ros/src/styx_msgs/msg/Lane.msg")
-set(styx_msgs_SERVICE_FILES "")
